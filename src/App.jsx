336359004a8bb607faac58f283e648506e2c5fee
Aman <amangupta71010@gmail.com>
2021-07-31 11:13:20 +0530

    dynamic topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import "./app.css"
+import { useState } from "react";
 import Topbar from "./components/topbar/Topbar"
 import Intro from "./components/intro/Intro"
 import Work from "./components/work/Work"
 import Portfolio from "./components/portfolio/Portfolio"
 import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
-import "./app.css"
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar 
+        menuOpen = {menuOpen}
+        setMenuOpen = {setMenuOpen}
+      />
       <div className="sections">
         <Intro />
         <Work />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,33 @@
-import './topbar.css'
+import "./topbar.css"
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import EmailIcon from '@material-ui/icons/Email';
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar" + (menuOpen ? " active": "")}>
+            <div className="wrapper">
+
+                <div className="left">
+                    <a href="#intro" className="logo">ag.</a>
+                    <div className="itemcontainer">
+                        <PersonAddIcon className="icon"/>
+                        <span>+91 6287212463</span>
+                    </div>
+                    <div className="itemcontainer">
+                        <EmailIcon className="icon"/>
+                        <span>amangupta71010@gmail.com</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
