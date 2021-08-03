891e8e39fd1f024bf1f23e4f41e941e57fcd7d71
Aman <amangupta71010@gmail.com>
2021-08-03 10:51:20 +0530

    complete intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,35 @@
-import './intro.css'
+import "./intro.css"
+import { useEffect, useRef } from "react"
+import { init } from "ityped"
 
 export default function Intro() {
+    
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1000,
+            strings: ["Web Developer"] 
+        })
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
-                <div className="imgcontainer">
+                <div className="imgContainer">
                     <img src="assets/man.png" alt="personal-pic" />
                 </div>
             </div>
             <div className="right">
-                <div className="wrapper1">
+                <div className="wrapHeading">
                     <h2>Hi There, I'm</h2>
                     <h1>Aman Gupta</h1>
-                    <h3>Full Stack<span>Web Developer</span></h3>
+                    <h3>Full Stack <span ref={textRef}></span></h3>
                 </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="downArrowImg"/>
+                </a>
             </div>
         </div>
     )
