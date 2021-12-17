8297d027d2c0910c845a3341415a2562fbf34b80
aman71010 <amangupta71010@gmail.com>
2021-12-17 14:48:47 +0530

    added skills component

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,30 @@
+import './skills.css'
+import { skills } from '../../data'
+
+export default function Skills() {
+    return (
+        <div className="skills" id="skills">
+            <div className="skills-wrapper">
+                <div className="s-left">
+                    <img className="skills-img" src="assets/SKILLS.PNG" alt="" />
+                </div>
+                <div className="s-right">
+                    <h1 className="s-heading">Skills</h1>
+                    <div className="s-container">
+                        { skills.map((skill) => (
+                            <div className="s-box">
+                                <div className="s-circle">   
+                                    <span class="iconify" data-icon={skill.fontAwesomeClassname} data-width="48" data-height="48"></span>
+                                </div>
+                                <div className="s-name">
+                                    {skill.skillName}
+                                </div>
+                            </div>
+                        ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
