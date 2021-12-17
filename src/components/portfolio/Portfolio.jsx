c2f640fb88d371dfc78cad6039d50a0f702929b8
aman71010 <amangupta71010@gmail.com>
2021-12-17 15:04:26 +0530

    modified and make responsive

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,24 @@
 import './portfolio.css'
+import { products } from '../../data'
+import Product from '../product/Product'
 
 export default function Portfolio() {
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <div className="p-texts">
+                <h1 className="p-header">Portfolio</h1>
+            </div>
+            <div className="p-lists">
+               { products.map((item) => (
+                    <Product 
+                        key = {item.id}
+                        link = {item.link}
+                        img = {item.img}
+                    />
+                ))}
+
+            </div>
         </div>
     )
 }
