04d0bfea56887936cfb2b7f43b0c150b59390386
aman71010 <amangupta71010@gmail.com>
2021-12-17 15:02:56 +0530

    modified and make responsive

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,9 @@
 import "./home.css"
 import { useEffect, useRef } from "react"
 import { init } from "ityped"
+import { resumeLink } from '../../data'
+import SocialIcon from '../socialIcon/SocialIcon'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
 
 export default function Home() {
     
@@ -18,7 +21,7 @@ export default function Home() {
         <div className="home" id="home">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/man.png" alt="personal-pic" />
+                    <img src="assets/homeimg-removebg new.png" alt="personal-pic" />
                 </div>
             </div>
             <div className="right">
@@ -27,9 +30,19 @@ export default function Home() {
                     <h1>Aman Gupta</h1>
                     <h3><span ref={textRef}></span></h3>
                 </div>
-                <a href="#portfolio">
-                    <img src="assets/down.png" alt="downArrowImg"/>
-                </a>
+                <div style={{margin: "20px 50px"}}>
+                    <SocialIcon />
+                </div>
+                <div className="resume-btn">
+                    <a className="resume-link" href={resumeLink} target="_blank" rel="noreferrer">
+                        <button className="btn"><FileCopyIcon style={{paddingRight: "5px"}}/>My Resume</button>
+                    </a>
+                </div>
+                <div>
+                    <a className="a-downarrow" href="#portfolio">
+                        <img src="assets/down.png" alt="downArrowImg"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
